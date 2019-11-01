4b3e3e9d5af895117bfeca640c7b54472073ff61
Volker Nauruhn <nauruhn@cynatix.com>
2019-11-01 16:39:28 +0100

    syntax and typo fix in I18nLangDecorator

--- src/lib/decorators/i18n-lang.decorator.ts
+++ src/lib/decorators/i18n-lang.decorator.ts
@@ -4,7 +4,7 @@ export const I18nLang = createParamDecorator((data, req) => {
   // this is gonna be so nasty..
   // FIXME: This has to be fixed in later stages! PLEASE!
   if (Array.isArray(req)) {
-    return resolveI18nLangaugeFromGraphQLContext(req);
+    return resolveI18nLanguageFromGraphQLContext(req);
   }
   return resolveI18nLanguageFromRestRequest(req);
 });
@@ -13,7 +13,7 @@ function resolveI18nLanguageFromRestRequest(req) {
   return req.i18nLang || (req.raw ? req.raw.i18nLang : undefined);
 }
 
-function resolveI18nLangaugeFromGraphQLContext(req) {
-  const [root, args, ctx, info] = req;
+function resolveI18nLanguageFromGraphQLContext(req) {
+  const { ctx } = req;
   return ctx.req.i18nLang;
 }

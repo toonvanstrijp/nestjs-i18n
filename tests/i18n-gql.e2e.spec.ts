ceb43eb0faf04bac4009d0ba43bf0c45f00483fe
Toon van Strijp <toonvanstrijp@gmail.com>
2020-09-24 23:37:17 +0200

    update dependencies

--- tests/i18n-gql.e2e.spec.ts
+++ tests/i18n-gql.e2e.spec.ts
@@ -15,10 +15,9 @@ import { CatModule } from './app/cats/cat.module';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 import * as WebSocket from 'ws';
 import ApolloClient from 'apollo-client';
-import { WebSocketLink } from "apollo-link-ws";
+import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
-import { async } from 'rxjs';
 
 describe('i18n module e2e graphql', () => {
   let app: INestApplication;
@@ -35,7 +34,7 @@ describe('i18n module e2e graphql', () => {
             'en-CA': 'fr',
             'en-*': 'en',
             'fr-*': 'fr',
-            'pt': 'pt-BR',
+            pt: 'pt-BR',
           },
           resolvers: [
             new HeaderResolver(['x-custom-lang']),
@@ -50,7 +49,8 @@ describe('i18n module e2e graphql', () => {
         GraphQLModule.forRoot({
           installSubscriptionHandlers: true,
           typePaths: ['*/**/*.graphql'],
-          context: ({ req, connection }) => connection ? { req: connection.context } : { req },
+          context: ({ req, connection }) =>
+            connection ? { req: connection.context } : { req },
           path: '/graphql',
         }),
         CatModule,
@@ -60,9 +60,21 @@ describe('i18n module e2e graphql', () => {
 
     app = module.createNestApplication();
     await app.listen(3000);
-    networkInterface = new SubscriptionClient('ws://localhost:3000/graphql', { reconnect: true, connectionParams: {headers: {'x-custom-lang': 'fr'}} }, WebSocket);
-    networkInterface.onError((error) => { console.log('error', error) });
-    apollo = new ApolloClient({ link: new WebSocketLink(networkInterface), cache: new InMemoryCache() })
+    networkInterface = new SubscriptionClient(
+      'ws://localhost:3000/graphql',
+      {
+        reconnect: true,
+        connectionParams: { headers: { 'x-custom-lang': 'fr' } },
+      },
+      WebSocket,
+    );
+    networkInterface.onError((error) => {
+      console.log('error', error);
+    });
+    apollo = new ApolloClient({
+      link: new WebSocketLink(networkInterface),
+      cache: new InMemoryCache(),
+    });
   });
 
   it(`should query a particular cat in NL`, () => {
@@ -317,28 +329,43 @@ describe('i18n module e2e graphql', () => {
   });
 
   it(`should subscribe to catAdded and return cat name with "fr" placeholder`, async (done) => {
-    apollo.subscribe({
-      query: gql`subscription catAdded {
-     catAdded
-    }`}).subscribe({ next: catText => { expect(catText).toEqual({"data": {"catAdded": "Chat: Haya"}}) }, error: error => {throw error} });
+    apollo
+      .subscribe({
+        query: gql`
+          subscription catAdded {
+            catAdded
+          }
+        `,
+      })
+      .subscribe({
+        next: (catText) => {
+          expect(catText).toEqual({ data: { catAdded: 'Chat: Haya' } });
+        },
+        error: (error) => {
+          throw error;
+        },
+      });
 
     await request(app.getHttpServer())
       .post('/graphql')
       .send({
         operationName: null,
         variables: {},
-        query: 'mutation {  createCat(createCatInput: {name: "Haya", age: 2})  { name, age }  }',
+        query:
+          'mutation {  createCat(createCatInput: {name: "Haya", age: 2})  { name, age }  }',
       })
       .expect(200, {
         data: {
           createCat: {
             name: 'Haya',
-            age: 2
+            age: 2,
           },
         },
       });
 
-    setTimeout(() => { done(); }, 2000);
+    setTimeout(() => {
+      done();
+    }, 2000);
   });
 
   afterAll(async () => {

5d4cb9a08c9fb98288e57d80febbde20e1b197b7
toonvanstrijp <toonvanstrijp@gmail.com>
2022-03-24 19:56:40 +0100

    WIP: docs

---
+++ docusaurus.config.js
@@ -0,0 +1,69 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'nestjs-i18n',
+  tagline: 'The i18n module for Nest.',
+  url: 'https://nestjs-i18n.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'toonvanstrijp', // Usually your GitHub org/user name.
+  projectName: 'nestjs-i18n', // Usually your repo name.
+  trailingSlash: false,
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/toonvanstrijp/nestjs-i18n/tree/main',
+        },
+        blog: false
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'nestjs-i18n',
+        items: [
+          {
+            href: 'https://github.com/toonvanstrijp/nestjs-i18n',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ToonvanStrijp/nestjs-i18n',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;

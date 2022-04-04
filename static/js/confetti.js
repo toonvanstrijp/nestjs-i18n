155ccd1a2fd054b333696086e49cbd53dc17fbb0
Toon van Strijp <toonvanstrijp@gmail.com>
2022-04-04 22:56:45 +0200

    fix theme change observer

--- static/js/confetti.js
+++ static/js/confetti.js
@@ -1,8 +1,32 @@
+var observeDOM = (function () {
+  var MutationObserver =
+    window.MutationObserver || window.WebKitMutationObserver;
+
+  return function (obj, callback) {
+    if (!obj || obj.nodeType !== 1) return;
+
+    if (MutationObserver) {
+      // define a new observer
+      var mutationObserver = new MutationObserver(callback);
+
+      // have the observer observe foo for changes in children
+      mutationObserver.observe(obj, { childList: true, subtree: true });
+      return mutationObserver;
+    }
+
+    // browser support fallback
+    else if (window.addEventListener) {
+      obj.addEventListener('DOMNodeInserted', callback, false);
+      obj.addEventListener('DOMNodeRemoved', callback, false);
+    }
+  };
+})();
+
 function randomInRange(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-window.addEventListener('load', function () {
+function setHandler() {
   document.querySelector('img[alt="debugged"]').onclick = () => {
     confetti({
       angle: randomInRange(55, 125),
@@ -17,4 +41,12 @@ window.addEventListener('load', function () {
     audio.appendChild(popper);
     audio.play();
   };
+}
+
+window.addEventListener('load', function () {
+  observeDOM(
+    document.querySelector('.footer__bottom.text--center .margin-bottom--sm'),
+    setHandler,
+  );
+  setHandler();
 });

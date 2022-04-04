cfd51e6006e216d9d0df32b65228bdcb16c4ccf4
D056966 <steffen.schroeder01@sap.com>
2022-04-04 12:41:15 +0200

    Support Generic Type for Translate Method

--- src/i18n.context.ts
+++ src/i18n.context.ts
@@ -3,15 +3,15 @@ import { I18nService, TranslateOptions } from './services/i18n.service';
 export class I18nContext {
   constructor(readonly lang: string, private readonly service: I18nService) {}
 
-  public translate(key: string, options?: TranslateOptions) {
+  public translate<T = any>(key: string, options?: TranslateOptions): T {
     options = {
       lang: this.lang,
       ...options,
     };
-    return this.service.translate(key, options);
+    return this.service.translate<T>(key, options);
   }
 
-  public t(key: string, options?: TranslateOptions) {
-    return this.translate(key, options);
+  public t<T = any>(key: string, options?: TranslateOptions): T {
+    return this.translate<T>(key, options);
   }
 }

fdd63a4652c9c3aaf01d9c7caa1198f2fb0efb52
Yozan Adriel <lukadriel@gmail.com>
2023-08-22 09:41:22 +0000

    fix broken tests

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,5 +1,9 @@
 import { I18nOptionResolver } from '../interfaces/i18n-options.interface';
-import { ValidationArguments, ValidationError } from 'class-validator';
+import {
+  ValidationArguments,
+  ValidationError,
+  getMetadataStorage,
+} from 'class-validator';
 import {
   I18nValidationError,
   I18nValidationException,
@@ -13,8 +17,12 @@ export function shouldResolve(e: I18nOptionResolver) {
 }
 
 function validationErrorToI18n(e: ValidationError): I18nValidationError {
+  console.log('target: ', JSON.stringify(e.target));
   return {
     property: e.property,
+    value: e.value,
+    target: e.target,
+    contexts: e.contexts,
     children: e?.children?.map(validationErrorToI18n),
     constraints: !!e.constraints
       ? Object.keys(e.constraints).reduce((result, key) => {
@@ -56,13 +64,34 @@ export function formatI18nErrors<K = Record<string, unknown>>(
   options?: TranslateOptions,
 ): I18nValidationError[] {
   return errors.map((error) => {
+    const limits = getMetadataStorage()
+      .getTargetValidationMetadatas(
+        error.target.constructor,
+        error.target.constructor.name,
+        true,
+        false,
+      )
+      .find(
+        (meta) =>
+          meta.target === error.target.constructor &&
+          meta.propertyName === error.property,
+      );
+    console.log('limits: ', limits);
+    const constraints = Object.assign({}, limits?.constraints);
     error.children = formatI18nErrors(error.children ?? [], i18n, options);
     error.constraints = Object.keys(error.constraints).reduce((result, key) => {
       const [translationKey, argsString] = error.constraints[key].split('|');
       const args = !!argsString ? JSON.parse(argsString) : {};
       result[key] = i18n.translate(translationKey as Path<K>, {
         ...options,
-        args: { property: error.property, ...args },
+        args: {
+          property: error.property,
+          value: error.value,
+          target: error.target,
+          contexts: error.contexts,
+          constraints: constraints,
+          ...args,
+        },
       });
       return result;
     }, {});

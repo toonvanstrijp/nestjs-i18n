f392ae2b8748fc4a1423c0c1071a726379623ef6
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-04-22 22:02:01 +0545

    refactor: streamline i18n context handling and improve utility functions

--- src/types.ts
+++ src/types.ts
@@ -9,18 +9,17 @@ type PathKeyOf<T> = T extends any[]
   ? Exclude<Extract<keyof T, string>, keyof any[]>
   : Extract<keyof T, string>;
 
-export type Path<T, Depth extends 1[] = []> =
-  [T] extends [Record<string, any>]
-    ? {
-        [K in PathKeyOf<T>]: 0 extends 1 & T[K]
-          ? never
-          : [T[K]] extends [Record<string, any>]
-            ? Depth['length'] extends 8
-              ? K
-              : K | `${K}.${Path<T[K], [...Depth, 1]>}`
-            : K;
-      }[PathKeyOf<T>]
-    : never;
+export type Path<T, Depth extends 1[] = []> = [T] extends [Record<string, any>]
+  ? {
+      [K in PathKeyOf<T>]: 0 extends 1 & T[K]
+        ? never
+        : [T[K]] extends [Record<string, any>]
+          ? Depth['length'] extends 8
+            ? K
+            : K | `${K}.${Path<T[K], [...Depth, 1]>}`
+          : K;
+    }[PathKeyOf<T>]
+  : never;
 
 export type PathValue<T, P extends string> = P extends `${infer Key}.${infer Rest}`
   ? Key extends keyof T
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export * from './context';
 export * from './file';
 export * from './format';
+export * from './i18n-context';
 export * from './merge';
+export * from './resolver';
 export * from './util';

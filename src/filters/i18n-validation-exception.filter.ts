852e0d359510471420ea9778ecca278d0c4ee0ad
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-04-28 10:25:25 +0545

    fix: remove iterare dep with native

--- src/filters/i18n-validation-exception.filter.ts
+++ src/filters/i18n-validation-exception.filter.ts
@@ -4,7 +4,7 @@ import {
   ExceptionFilter,
   ValidationError,
 } from '@nestjs/common';
-import iterate from 'iterare';
+
 import { I18nContext } from '..';
 import {
   I18nValidationError,
@@ -115,13 +115,12 @@ export class I18nValidationExceptionFilter implements ExceptionFilter {
   protected flattenValidationErrors(
     validationErrors: ValidationError[],
   ): string[] {
-    return iterate(validationErrors)
+    return validationErrors
       .map((error) => mapChildrenToValidationErrors(error))
-      .flatten()
+      .flat()
       .filter((item) => !!item.constraints)
       .map((item) => Object.values(item.constraints ?? {}))
-      .flatten()
-      .toArray();
+      .flat();
   }
   protected buildResponseBody(
     host: ArgumentsHost,
